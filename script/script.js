31f07dd3f53bde4d8fd3fb381dd5093edb9f8e96
vitorsoaresf <vitorsoaresf@gmail.com>
2021-10-05 08:57:10 -0300

    organiza estilo do codigo

--- script/script.js
+++ script/script.js
@@ -1,4 +1,44 @@
-//escrito por André
+// VARIAVEIS
+
+const main = document.getElementById('app');
+
+let isPlayer1 = true
+let objCol = {
+    coluna0: 0,
+    coluna1: 0,
+    coluna2: 0,
+    coluna3: 0,
+    coluna4: 0,
+    coluna5: 0,
+    coluna6: 0
+}
+
+// FUNÇÕES
+
+const aplicacao = () => {
+    novoJogo();
+}
+
+const addDisco = (event) => {
+    let col = event.target.closest('.coluna');
+    if (col === null || col === undefined) {
+        return
+    }
+    const idCol = col.id;
+    if (objCol[idCol] === 6) {
+        console.log('NEIN!!!')
+    } else {
+        let celula = col.children[col.children.length - objCol[idCol] - 1];
+        if (celula.lastChild === null) {
+            objCol[idCol]++;
+            const disco = document.createElement('div');
+            disco.classList.add('discoPlayer2');
+            celula.appendChild(disco);
+            celula = col.children[col.children.length - objCol[idCol] - 1];
+        }
+    }
+}
+
 const criarColuna = (id, id1) => {
     const mainTag = document.getElementsByTagName('main')[0];
 
@@ -21,224 +61,35 @@ const criarColuna = (id, id1) => {
     }
 }
 
-for (let i = 0; i <= 6; i++) {
-    let l = []
-    let newI = ''
-    let newJ = ''
-    for (let j = 0; j <= 5; j++) {
-        newI = i.toString();
-        newJ = j.toString();
-        l.push(newI + newJ)
-    }
-    criarColuna(i, l)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// escrito por Erivan
-let isPlayer1 = true
-function getPlayer() {
+const getPlayer = () => {
     if (isPlayer1) {
         return "Player1"
     } else {
         return "Player2"
     }
 }
-function mudaPlayer() {
+
+const mudaPlayer = () => {
     isPlayer1 = !isPlayer1
 }
 
-
-
-
-
-
-
-
-
-
-
-// Escrito por Vitor
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// escrito por Daniel
-const main = document.getElementById('app');
-main.addEventListener('click', addDisco);
-let linha = 1;
-
-let objCol = {
-    coluna0: 0,
-    coluna1: 0,
-    coluna2: 0,
-    coluna3: 0,
-    coluna4: 0,
-    coluna5: 0,
-    coluna6: 0
-}
-
-function addDisco(event) {
-    let col = event.target.closest('.coluna');
-    if (col === null || col === undefined) {
-        return
-    }
-    const idCol = col.id;
-    if (objCol[idCol] === 6) {
-        console.log('NEIN!!!')
-    } else {
-        let celula = col.children[col.children.length - objCol[idCol] - 1];
-        if (celula.lastChild === null) {
-            objCol[idCol]++;
-            const disco = document.createElement('div');
-            disco.classList.add('discoPlayer2');
-            celula.appendChild(disco);
-            celula = col.children[col.children.length - objCol[idCol] - 1];
+const novoJogo = () => {
+    for (let i = 0; i <= 6; i++) {
+        let l = []
+        let newI = ''
+        let newJ = ''
+        for (let j = 0; j <= 5; j++) {
+            newI = i.toString();
+            newJ = j.toString();
+            l.push(newI + newJ)
         }
+        criarColuna(i, l)
     }
 }
+
+
+// EVENTOS
+
+main.addEventListener('click', addDisco);
+
+aplicacao();
